3d469f42b15b7aac7fb6b4f1b60ecb2e70cc8571
GDharan07 <dharansparrow77@gmail.com>
2024-08-03 14:08:58 +0530

    added register and login route

--- backend/Routes/UserRouter.js
+++ backend/Routes/UserRouter.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 
 router.post('/register',async(req,res)=>{
     try{
-        const{email,name,password,role} = req.body;
+        const{email,name,password,department} = req.body;
         const userEmail = await userSchema.findOne({email});
         const userName = await userSchema.findOne({name});
 
@@ -16,13 +16,38 @@ router.post('/register',async(req,res)=>{
             return res.json("user already exits")
         }
 
+        if(department === null || department === undefined || department === ""){
+            return res.json("department is empty")
+        } 
+
         const hashPassword = await bcrypt.hash(password,10)
-        const userData = await new userSchema({email,name,role,password:hashPassword})
+        const userData = await new userSchema({email,name,department,password:hashPassword})
         await userData.save();
-        // res.json('userposted successfully')
+        res.json('userposted successfully')
     }catch(err){
         console.log(err)
     }
-})
+}) 
+
+
+router.post('/login',async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        const user = await userSchema.findOne({email})
+        if(!user){
+            return res.json("User not Found")
+        }
+        const validPassword = await bcrypt.compareSync(password,user.password);
+        if(!validPassword){
+            return res.json("Mismatch Password");
+        } 
+
+        const token = jwt.sign({ id: user.id }, "secret");
+        res.json({ token});
+
+    }catch(err){
+        console.log(err)
+    }
+}) 
 
 module.exports = router;
