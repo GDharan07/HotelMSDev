4f7abf4bc1c5b83094b124d7ae8d5aea701e6360
GDharan07 <dharansparrow77@gmail.com>
2024-08-04 18:04:08 +0530

    login page design created

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import img from "../assets/colorfullimg.jpg" 
 
 function Login() {
   return (
     <div className='container'>
-     <div className='container-fluid'>
-        <div className=''>
-          <img></img>
+        <div className='row mt-5'>
+            <div className='col-lg-5 col-md-6'>
+                <img src={img} alt='alt' className='rounded' height="450vh" width='400px'/>
+            </div>
+            <div className='col-lg-5 col-md-6'>
+                <div className='d-flex flex-column align-items-center mt-5'>
+                    <div className='login-inputs mb-3'>
+                        <input type='text' class='form-control' placeholder='Username'/>
+                    </div>
+                    <div className='login-inputs'>
+                        <input type='password' class='form-control' placeholder='Password'/>
+                    </div> 
+                </div>
+            </div>
         </div>
-     </div>
-    </div>
+    </div>  
   )
 }
 
